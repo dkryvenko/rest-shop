8ce7fa7997fa43d24714d712c4fbb9dbabe8def4
dmytro.kryvenko <p4$$w07d>
2013-02-21 12:33:25 +0200

    added additional properties to Product bean

--- src/main/java/com/dynamo/shop/model/Product.java
+++ src/main/java/com/dynamo/shop/model/Product.java
@@ -6,15 +6,35 @@ package com.dynamo.shop.model;
  */
 public class Product {
 
+    public static final String PRODUCT_TYPE_PIZZA = "pizza";
+    public static final String PRODUCT_TYPE_BEVERAGE = "beverage";
+    public static final String PRODUCT_TYPE_FOOD = "food";
+
+    private String id;
+    private String type;
     private String name;
     private String description;
+    private double weight;
+    private double size;
+    private double price;
+    private String smallThumb;
+    private String mediumThumb;
+    private String largeThumb;
 
-    public Product() {
+    public String getId() {
+        return id;
     }
 
-    public Product(String name, String description) {
-        this.name = name;
-        this.description = description;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getName() {
@@ -31,5 +51,53 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSmallThumb() {
+        return smallThumb;
+    }
+
+    public void setSmallThumb(String smallThumb) {
+        this.smallThumb = smallThumb;
+    }
+
+    public String getMediumThumb() {
+        return mediumThumb;
+    }
+
+    public void setMediumThumb(String mediumThumb) {
+        this.mediumThumb = mediumThumb;
+    }
+
+    public String getLargeThumb() {
+        return largeThumb;
+    }
+
+    public void setLargeThumb(String largeThumb) {
+        this.largeThumb = largeThumb;
     }
 }
